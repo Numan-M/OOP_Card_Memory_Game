53cab310cf3be61388e7f8ce9fc9079f1acbfab2
Numan-M <Numan_mahmood@hotmail.co.uk>
2023-02-05 14:54:09 +0000

    setting up

---
+++ main/cards.py
@@ -0,0 +1,9 @@
+# --PSEUDOCODE--
+# class card
+# holds a card
+# matched or not?
+# location
+# __eq__ to compare
+# __str__ to print card
+
+class Card:
---
+++ main/game.py
@@ -0,0 +1,15 @@
+# --PSEUDOCODE--
+# grid size
+# card options
+# columns
+# locations
+# methods
+    # create cards
+    # create grid
+    # check for matches
+    # check game won
+    # run game
+
+# dunder main
+    # create game instance
+    # call start game 
